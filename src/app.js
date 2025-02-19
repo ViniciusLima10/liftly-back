609f1692cb01c30529227faf36e4906202875266
Samuel de Souza Carvalho Almeida <you@example.com>
2025-02-19 02:50:17 +0000

    LIF-24: Adiciona relacionamento User-Gym e rotas de academias
    - Criação do modelo `UserGym` para associação entre `Users` e `Gyms`
    - Adiciona a migração `create-user-gyms.js` com chaves estrangeiras para `Users` e `Gyms`
    - Define relacionamentos `belongsToMany` entre `User` e `Gym` no Sequelize
    - Implementa as rotas `GET`, `POST`, `PUT` e `DELETE` para academias (`gymRoutes.js`)
    - Ajusta o `app.js` para registrar as novas rotas `/gyms`

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
+const gymRoutes = require('./routes/gymRoutes');
 
 const app = express();
 app.use(express.json());
 
 // Rotas
 app.use('/users', userRoutes);
+app.use('/gyms', gymRoutes);
 
 // Inicializar o servidor
 const PORT = process.env.PORT || 3001;
---
+++ src/models/userGym.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const UserGym = sequelize.define(
+      'UserGym',
+      {
+        userId: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+        },
+        gymId: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          references: {
+            model: 'Gyms',
+            key: 'id',
+          },
+        },
+      },
+      {
+        primaryKey: ['userId', 'gymId'], // Define a chave primária como uma chave composta
+      }
+    );
+  
+  UserGym.associate = function (models) {
+    models.User.belongsToMany(models.Gym, {
+      through: UserGym,
+      foreignKey: 'userId',
+      as: 'gyms',
+    });
+
+    models.Gym.belongsToMany(models.User, {
+      through: UserGym,
+      foreignKey: 'gymId',
+      as: 'users',
+    });
+  };
+
+  return UserGym;
+};
