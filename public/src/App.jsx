155dfb988059aa74043d193916bfbf4ce3a19c35
julianunes <julianunessouza@hotmail.com>
2025-02-20 10:19:53 -0300

    LIF-13: adicionando rota para gerenciar aulas

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/PageInicial";
 import LoginAluno from "./pages/LoginAluno";
 import LoginAdm from "./pages/LoginAdm";
 import LoginPersonal from "./pages/LoginPersonal";
+import ClassesAdm from "./pages/ClassesAdm";
 
 
 
@@ -20,6 +21,7 @@ function App() {
                 <Route path="/loginAluno" element={<LoginAluno />} />
                 <Route path="/loginAdm" element={<LoginAdm />} />
                 <Route path="/loginPersonal" element={<LoginPersonal />} />
+                <Route path="/EditarAulas" element={<ClassesAdm />} />
                 
         
               
