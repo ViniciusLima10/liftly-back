55a6618c5040069409f9ee8af83f4067bff37241
julianunes <julianunessouza@hotmail.com>
2025-02-20 10:12:19 -0300

    LIF-07-agendar-aulas: adicionando rota

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -7,6 +7,7 @@ import LoginAluno from "./pages/LoginAluno";
 import LoginAdm from "./pages/LoginAdm";
 import LoginPersonal from "./pages/LoginPersonal";
 import ClassesAluno from "./pages/Classes";
+import TreinoPage from "./pages/Treino";
 
 
 
@@ -22,13 +23,13 @@ function App() {
                 <Route path="/loginAdm" element={<LoginAdm />} />
                 <Route path="/loginPersonal" element={<LoginPersonal />} />
                 <Route path="/aulasAluno" element={<ClassesAluno />} />
-                
+                <Route path="/treino" element={<TreinoPage />} />
         
               
 
             </Routes>
         </Router>
     );
-}
+} 
 
 export default App;
