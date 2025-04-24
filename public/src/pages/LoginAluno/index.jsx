9dcf69c2f0533f6555af2316222d4b16db3d9701
Vinicius Lima <viniciusalima1007@gmail.com>
2025-04-24 08:54:24 -0300

    Removendo arquivos da pasta public e atualizando configs

--- public/src/pages/LoginAluno/index.jsx
+++
@@ -1,7 +0,0 @@
-import Login from "../../components/Login";
-
-const LoginAluno = () => {
-  return <Login tipoUsuario="aluno" />;
-};
-
-export default LoginAluno;
--- public/src/pages/LoginPersonal/index.jsx
+++
@@ -1,7 +0,0 @@
-import Login from "../../components/Login";
-
-const LoginPersonal = () => {
-  return <Login tipoUsuario="personal" />;
-};
-
-export default LoginPersonal;
